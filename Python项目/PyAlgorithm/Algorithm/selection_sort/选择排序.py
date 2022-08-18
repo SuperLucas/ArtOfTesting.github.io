c9dc2155aeb38f599a984fc08c01e6ad82e83aeb
dsLucas <a1733464820@gmail.com>
2022-08-18 18:21:42 +0800

    选择排序算法

--- Python项目/PyAlgorithm/Algorithm/selection_sort/选择排序.py
+++ Python项目/PyAlgorithm/Algorithm/selection_sort/选择排序.py
@@ -7,25 +7,27 @@
 # Gitee : https://gitee.com/SuperLucas/ArtOfTesting.github.io.git
 
 # 在此处下方键入你的内容：
-# 找到数组中的最小值
+# 函数findSmallest：找到数组中的最小值
 def findSmallest(arr):
-  # Stores the smallest value
-  smallest = arr[0]
-  # Stores the index of the smallest value
-  smallest_index = 0
-  for i in range(1, len(arr)):
-    if arr[i] < smallest:
-      smallest = arr[i]
-      smallest_index = i
-  return smallest_index
+    # 储存最小的值
+    smallest = arr[0]
+    # 储存最小的元素索引
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
 
-# Sort array
+
+# 数组排序
 def selectionSort(arr):
-  newArr = []
-  for i in range(len(arr)):
-      # Finds the smallest element in the array and adds it to the new array
-      smallest = findSmallest(arr)
-      newArr.append(arr.pop(smallest))
-  return newArr
+    newArr = []
+    for i in range(len(arr)):
+        # 找到这个最小值，并按顺序放到另一个新数组中
+        smallest = findSmallest(arr)
+        # pop() 参数为列表元素的索引值,pop()方法有返回值,返回的是列表对应的元素.
+        newArr.append(arr.pop(smallest))
+    return newArr
 
 print(selectionSort([5, 3, 6, 2, 10]))
